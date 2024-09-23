e424db643424b8fa6f873276c134b0de8a254e07
DESKTOP-FKODTS1\Admin <phamviethuy1210@gmail.com>
2024-09-24 03:29:38 +0700

    Init for EX8

---
+++ STM32Project/Core/Inc/exercise8.h
@@ -0,0 +1,17 @@
+/*
+ * exercise8.h
+ *
+ *  Created on: Sep 24, 2024
+ *      Author: Admin
+ */
+
+#ifndef INC_EXERCISE8_H_
+#define INC_EXERCISE8_H_
+
+#include "main.h"
+
+void init_ex8();
+void ex8_run();
+void setNumberOnClock(int num);
+
+#endif /* INC_EXERCISE8_H_ */
---
+++ STM32Project/Core/Src/exercise8.c
@@ -0,0 +1,67 @@
+/*
+ * exercise8.c
+ *
+ *  Created on: Sep 24, 2024
+ *      Author: Admin
+ */
+#include "exercise8.h"
+
+void init_ex8(){
+	HAL_GPIO_TogglePin(led0_GPIO_Port, led0_Pin);
+	HAL_GPIO_TogglePin(led1_GPIO_Port, led1_Pin);
+	HAL_GPIO_TogglePin(led2_GPIO_Port, led2_Pin);
+	HAL_GPIO_TogglePin(led3_GPIO_Port, led3_Pin);
+	HAL_GPIO_TogglePin(led4_GPIO_Port, led4_Pin);
+	HAL_GPIO_TogglePin(led5_GPIO_Port, led5_Pin);
+	HAL_GPIO_TogglePin(led6_GPIO_Port, led6_Pin);
+	HAL_GPIO_TogglePin(led7_GPIO_Port, led7_Pin);
+	HAL_GPIO_TogglePin(led8_GPIO_Port, led8_Pin);
+	HAL_GPIO_TogglePin(led9_GPIO_Port, led9_Pin);
+	HAL_GPIO_TogglePin(led10_GPIO_Port, led10_Pin);
+	HAL_GPIO_TogglePin(led11_GPIO_Port, led11_Pin);
+}
+void ex8_run(){
+
+}
+void setNumberOnClock(int num){
+	switch(num){
+			case 0:
+				HAL_GPIO_WritePin(led0_GPIO_Port, led0_Pin, GPIO_PIN_RESET);
+				break;
+			case 1:
+				HAL_GPIO_WritePin(led1_GPIO_Port, led1_Pin, GPIO_PIN_RESET);
+				break;
+			case 2:
+				HAL_GPIO_WritePin(led2_GPIO_Port, led2_Pin, GPIO_PIN_RESET);
+				break;
+			case 3:
+				HAL_GPIO_WritePin(led3_GPIO_Port, led3_Pin, GPIO_PIN_RESET);
+				break;
+			case 4:
+				HAL_GPIO_WritePin(led4_GPIO_Port, led4_Pin, GPIO_PIN_RESET);
+				break;
+			case 5:
+				HAL_GPIO_WritePin(led5_GPIO_Port, led5_Pin, GPIO_PIN_RESET);
+				break;
+			case 6:
+				HAL_GPIO_WritePin(led6_GPIO_Port, led6_Pin, GPIO_PIN_RESET);
+				break;
+			case 7:
+				HAL_GPIO_WritePin(led7_GPIO_Port, led7_Pin, GPIO_PIN_RESET);
+				break;
+			case 8:
+				HAL_GPIO_WritePin(led8_GPIO_Port, led8_Pin, GPIO_PIN_RESET);
+				break;
+			case 9:
+				HAL_GPIO_WritePin(led9_GPIO_Port, led9_Pin, GPIO_PIN_RESET);
+				break;
+			case 10:
+				HAL_GPIO_WritePin(led10_GPIO_Port, led10_Pin, GPIO_PIN_RESET);
+				break;
+			case 11:
+				HAL_GPIO_WritePin(led11_GPIO_Port, led11_Pin, GPIO_PIN_RESET);
+				break;
+			default:
+				break;
+		}
+}
