3578c56e0aae20614fe431cf56ab56266a9a864b
DESKTOP-FKODTS1\Admin <phamviethuy1210@gmail.com>
2024-09-24 10:19:42 +0700

    Init for EX10

---
+++ STM32Project/Core/Inc/exercise10.h
@@ -0,0 +1,16 @@
+/*
+ * exercise10.h
+ *
+ *  Created on: Sep 24, 2024
+ *      Author: Admin
+ */
+
+#ifndef INC_EXERCISE10_H_
+#define INC_EXERCISE10_H_
+
+#include "main.h"
+
+void init_ex10();
+void ex10_run();
+
+#endif /* INC_EXERCISE10_H_ */
